88a9495718a8644d4b97806164278ff2672e6fe3
Phi Hai Nguyen <45500630+phihainguyen@users.noreply.github.com>
2019-05-07 15:52:22 -0700

    Login (#10)
    * updating the webpage
    
    * updating the components
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,6 +5,14 @@ const url = "http://localhost:3030";
 export default {
   getUsers: function() {
     return axios.get(url + "/users");
+  },
+  createNewUser: userObj => {
+    return axios.post(`${url}/auth/${userObj.account_type}/signup`, {
+      name: userObj.name,
+      email: userObj.email,
+      password: userObj.password,
+      address: userObj.address
+    });
   }
   // getSavedBooks: function() {
   //   return axios.get(url + "/api/strains");
